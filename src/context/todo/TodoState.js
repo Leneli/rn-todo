9697b81951b27de066222fc814afe6a1bf834767
Elena Kuklina <kuklina@mail.tvil.ru>
2020-09-09 12:42:30 +0400

    Steps 43-44

---
+++ src/context/todo/TodoState.js
@@ -0,0 +1,15 @@
+import React, { useReducer } from 'react';
+import { TodoContext } from './todoContext';
+import { todoReducer } from './todoReducer';
+
+const INITIAL_STATE = {
+  todoItems: [
+    { id: 1, title: 'Выучить React Native!' }
+  ],
+};
+
+export const TodoState = ({ children }) => {
+  const [state, dispatch] = useReducer(todoReducer, INITIAL_STATE);
+
+  return <TodoContext.Provider value={{ todoItems: state.todoItems }}>{children}</TodoContext.Provider>;
+}
